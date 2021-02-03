62abc1885d2bfbd777f3fe6f965aecb792db7405
Evgenii <mixalev10a@gmail.com>
2021-02-03 21:36:35 +0300

    Оценка поледних просмотренних фильмов

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 "use strict";
+
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?","");
+
+
+const personLastMovie = prompt("Один из последних просмотренных фильмов?","");
+const personRatingMovie = prompt("На сколько оцените его?","");
+// for(let i = 0; i > +numberOfFilms ;i++) {
+    
+// }
+
+
+
+
+const peronalMovieDB = {
+
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privet: false
+
+};
+peronalMovieDB.movies[personLastMovie] = personRatingMovie;
+
+
+
+
+
+console.log( peronalMovieDB );

9666539bf9fdfc7205e6b5ceb6c7596b2d2b2213
Evgenii <mixalev10a@gmail.com>
2021-02-03 21:42:10 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 "use strict";
 
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?","");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
 
 
 const personLastMovie = prompt("Один из последних просмотренных фильмов?","");
 const personRatingMovie = prompt("На сколько оцените его?","");
+const personLastMovie2 = prompt("Один из последних просмотренных фильмов?","");
+const personRatingMovie2 = prompt("На сколько оцените его?","");
 // for(let i = 0; i > +numberOfFilms ;i++) {
     
 // }
@@ -22,6 +24,7 @@ const peronalMovieDB = {
 
 };
 peronalMovieDB.movies[personLastMovie] = personRatingMovie;
+peronalMovieDB.movies[personLastMovie2] = personRatingMovie2;
 
 
 

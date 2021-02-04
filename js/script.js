9665afba3e20aaeb1886bc38818aaaf01e2901c3
Evgenii <mixalev10a@gmail.com>
2021-02-04 08:55:36 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,16 +3,20 @@
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
 
 
-const personLastMovie = prompt("Один из последних просмотренных фильмов?","");
-const personRatingMovie = prompt("На сколько оцените его?","");
-const personLastMovie2 = prompt("Один из последних просмотренных фильмов?","");
-const personRatingMovie2 = prompt("На сколько оцените его?","");
-// for(let i = 0; i > +numberOfFilms ;i++) {
-    
-// }
 
-
-
+if (numberOfFilms > 0 && numberOfFilms < 10) {
+    alert("Просмотрено давольно мало фильмов");
+} else if (numberOfFilms >= 10 && numberOfFilms < 30) {
+    alert("Вы классический зритель");
+} else if (numberOfFilms >= 30){
+    alert("Вы киноман");
+} else if (numberOfFilms === 0 ){
+    alert("Как так?");
+} else if (numberOfFilms == ""){
+    alert("Введите данные");
+} else {
+    alert("Произошла ошибка");
+}
 
 const peronalMovieDB = {
 
@@ -23,11 +27,35 @@ const peronalMovieDB = {
     privet: false
 
 };
-peronalMovieDB.movies[personLastMovie] = personRatingMovie;
-peronalMovieDB.movies[personLastMovie2] = personRatingMovie2;
 
+for(let i = 0; i < numberOfFilms; i++){
+    let a = prompt("Один из последних просмотренных фильмов?","");
+    let aa = +prompt("На сколько оцените его?","");
 
+    // if (a.length > 50) {
+    //     break;
+    // } else {
+    //     false
+    // }
 
+    peronalMovieDB.movies[a] = aa;
+}
+
+// if (a > )
+
+if (numberOfFilms > 0 && numberOfFilms < 10) {
+    alert("Просмотрено давольно мало фильмов");
+} else if (numberOfFilms >= 10 && numberOfFilms < 30) {
+    alert("Вы классический зритель");
+} else if (numberOfFilms >= 30){
+    alert("Вы киноман");
+} else if (numberOfFilms === 0 ){
+    alert("Как так?");
+} else if (numberOfFilms == ""){
+    alert("Введите данные");
+} else {
+    alert("Произошла ошибка");
+}
 
 
 console.log( peronalMovieDB );

12c3b7e0ae7966fc7e26faa8b008fe82a10ab85d
Evgenii <mixalev10a@gmail.com>
2021-02-05 07:15:02 +0300

    Вопрос через цикл с проверкой данных

--- js/script.js
+++ js/script.js
@@ -32,27 +32,22 @@ for(let i = 0; i < numberOfFilms; i++){
     let a = prompt("Один из последних просмотренных фильмов?","");
     let aa = +prompt("На сколько оцените его?","");
 
-    // if (a.length > 50) {
-    //     break;
-    // } else {
-    //     false
-    // }
-
-    peronalMovieDB.movies[a] = aa;
+    if ( a != null && aa != null && a != "" && aa != "" && a.length < 50) {
+        peronalMovieDB.movies[a] = aa;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+    
 }
 
-// if (a > )
 
-if (numberOfFilms > 0 && numberOfFilms < 10) {
+if (peronalMovieDB.count > 0 && peronalMovieDB.count < 10) {
     alert("Просмотрено давольно мало фильмов");
-} else if (numberOfFilms >= 10 && numberOfFilms < 30) {
-    alert("Вы классический зритель");
-} else if (numberOfFilms >= 30){
-    alert("Вы киноман");
-} else if (numberOfFilms === 0 ){
-    alert("Как так?");
-} else if (numberOfFilms == ""){
-    alert("Введите данные");
+} else if (peronalMovieDB.count >= 10 && peronalMovieDB.count < 30) {
+    alert("Вы классический зритель");   
+} else if (peronalMovieDB.count >= 30) {
 } else {
     alert("Произошла ошибка");
 }

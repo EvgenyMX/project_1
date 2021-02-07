85b97300f68bdd27bfd0ba40ecf274704123a050
Evgenii <mixalev10a@gmail.com>
2021-02-07 20:56:50 +0300

    Ответить на вопрос-любимый жанр 3 раза

--- js/script.js
+++ js/script.js
@@ -1,51 +1,60 @@
 "use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
+let numberOfFilms;
+function start() { 
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
+    while( numberOfFilms === null || numberOfFilms === "" || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
+    }
+}
+start();
 const peronalMovieDB = {
-
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
-
 };
 
-for(let i = 0; i < numberOfFilms; i++){
-    let a = prompt("Один из последних просмотренных фильмов?","");
-    let aa = +prompt("На сколько оцените его?","");
 
-    if ( a != null && aa != null && a != "" && aa != "" && a.length < 50) {
-        peronalMovieDB.movies[a] = aa;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }
+function rememberMyFilms() { 
+    for(let i = 0; i < numberOfFilms; i++){
+        let a = prompt("Один из последних просмотренных фильмов?","");
+        let aa = +prompt("На сколько оцените его?","");
     
+        if ( a != null && aa != null && a != "" && aa != "" && a.length < 50) {
+            peronalMovieDB.movies[a] = aa;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+        
+    }
 }
+rememberMyFilms();
 
-if (peronalMovieDB.count > 0 && peronalMovieDB.count < 10) {
-    alert("Просмотрено давольно мало фильмов");
-} else if (peronalMovieDB.count >= 10 && peronalMovieDB.count < 30) {
-    alert("Вы классический зритель");   
-} else if (peronalMovieDB.count >= 30) {
-    alert("Вы киноман");
-} else {
-    alert("Произошла ошибка");
+function detectPersonalLevel() {
+    if (peronalMovieDB.count > 0 && peronalMovieDB.count < 10) {
+        alert("Просмотрено давольно мало фильмов");
+    } else if (peronalMovieDB.count >= 10 && peronalMovieDB.count < 30) {
+        alert("Вы классический зритель");   
+    } else if (peronalMovieDB.count >= 30) {
+        alert("Вы киноман");
+    } else {
+        alert("Произошла ошибка");
+    }
 }
 
 function writeYourGenres() { 
-    for ( let i = 0; i < 3; i++) {
-        const favoriteGenreText = `Ваш любимый жанр под номером ${i+1}`;
-        const favoriteGenre = prompt(favoriteGenreText);
-        peronalMovieDB.genres[i] = favoriteGenre;
+    for ( let i = 1; i < 3; i++) {
+        peronalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`);
     }
 }
 writeYourGenres();
 
-function showMyDB( p ) { 
-    if ( p === false) {
+function showMyDB( hiddenPrivat ) { 
+    if ( !hiddenPrivat ) {
         console.log( peronalMovieDB );
     } else {
         console.log( "Фильмы публичные" );
